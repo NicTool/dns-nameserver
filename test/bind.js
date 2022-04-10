11b66b2b83416fdb3338f9d537c9643814a13960
Matt Simerson <matt@tnpi.net>
2022-04-10 15:09:53 -0700

    style!: convert from CJS to ESM
    - reimplement parsers in pure JS (bye nearley & moo)
    - nsd: add value cleanups
    - knot: handle array-type items
    - test(bind): greatly improved test
    - feat: start on nt-ns.js

--- test/bind.js
+++ test/bind.js
@@ -1,8 +1,8 @@
 
-const assert = require('assert')
-const fs     = require('fs').promises
+import assert from 'assert'
+import fs     from 'fs/promises'
 
-const ns = require('../lib/bind.js')
+import { parseConfig } from '../lib/bind.js'
 
 
 describe('bind', function () {
@@ -13,9 +13,52 @@ describe('bind', function () {
       const file = './test/fixtures/bind/named.conf-ztrax-master'
       const buf = await fs.readFile(file)
 
-      const r = await ns.parseConfig(buf.toString())
+      const r = await parseConfig(buf.toString())
       // console.dir(r, { depth: null })
-      assert.equal(r.length, 5)
+      assert.deepStrictEqual(r, {
+        options: {
+          directory        : '/var/named',
+          version          : 'get lost',
+          'allow-transfer' : '{"none";}',
+          'allow-recursion': '{192.168.3.0/24;}',
+        },
+        logging: {
+          'channel example_log': {
+            file            : '"/var/log/named/example.log" versions 3 size 2m',
+            severity        : 'info',
+            'print-severity': 'yes',
+            'print-time'    : 'yes',
+            'print-category': 'yes',
+          },
+          'category default': { example_log: '' },
+        },
+        zone: [
+          { name: '.', type: 'hint', file: 'root.servers' },
+          {
+            name            : 'example.com',
+            type            : 'master',
+            file            : 'master/master.example.com',
+            'allow-transfer': '{192.168.23.1;192.168.23.2;}',
+          },
+          {
+            name                 : 'localhost',
+            type                 : 'master',
+            file                 : 'master.localhost',
+            'allow-update{none;}': '',
+          },
+          {
+            name                 : '0.0.127.in-addr.arpa',
+            type                 : 'master',
+            file                 : 'localhost.rev',
+            'allow-update{none;}': '',
+          },
+          {
+            name: '0.168.192.IN-ADDR.ARPA',
+            type: 'master',
+            file: '192.168.0.rev',
+          },
+        ],
+      })
     })
   })
 
