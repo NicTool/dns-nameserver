5056fe168f6be18c1af8e7f937b3f4a94e693e41
Matt Simerson <matt@tnpi.net>
2026-03-28 12:13:41 -0700

    dep(eslint): upgrade to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:promise/recommended",
+        "plugin:node/recommended",
+    ),
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        ecmaVersion: 5,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-unused-vars": ["error", {
+            args: "none",
+        }],
+
+        "dot-notation": "error",
+        "prefer-const": "warn",
+    },
+}]);
